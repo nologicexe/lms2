8fe5d7f67a2897adea854461e3689a23f2acd904
nologicexe <nologicexe@gmail.com>
2020-12-25 07:35:59 +0300

    v0.1

--- lms2/settings.py
+++ lms2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '(u6jexg!%fbqm9+cm3n$na_ah1!^kabx^icgxax*4)n4w&og)@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
